a843ea83d71be2a8667b3fe8961043342409b155
József Erős <jozsef.eros@bitrise.io>
2018-11-30 14:24:10 +0100

    Use single quotes instead of doubles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { Beam } from "@bitrise/beam";
+import { Component, OnInit } from '@angular/core';
+import { Beam } from '@bitrise/beam';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
+  selector: 'app-root',
+  templateUrl: './app.component.html',
   styles: []
 })
 export class AppComponent implements OnInit {
-  title = "Test add-on";
+  title = 'Test add-on';
 
   ngOnInit() {
     Beam.init({
-      app_slug: "d08709ae5c5f6171",
-      app_name: "Fast building app"
+      app_slug: 'd08709ae5c5f6171',
+      app_name: 'Fast building app'
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { InlineSVGModule } from "ng-inline-svg";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
 
-import { AppComponent } from "./app.component";
-import { AppHeaderComponent } from "./app-header.component";
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header.component';
 
 @NgModule({
   declarations: [AppComponent, AppHeaderComponent],
