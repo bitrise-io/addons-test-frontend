32af00be7ca85c7d6695e48ad92c7436cd366dac
József Erős <jozsef.eros@bitrise.io>
2018-11-30 10:46:06 +0100

    Prettify code with linter

--- src/app/app-header.component.ts
+++ src/app/app-header.component.ts
@@ -1,11 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.scss']
+  selector: "app-header",
+  templateUrl: "./app-header.component.html",
+  styleUrls: ["./app-header.component.scss"]
 })
-
-export class AppHeaderComponent {
-
-}
+export class AppHeaderComponent {}
