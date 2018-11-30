2d783df6c48f5c804556c2037ead3f4cd28c875e
József Erős <jozsef.eros@bitrise.io>
2018-11-30 14:43:41 +0100

    Remove test for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { Beam } from '@bitrise/beam';
   styles: []
 })
 export class AppComponent implements OnInit {
-  title = 'Test add-on';
-
   ngOnInit() {
     Beam.init({
       app_slug: 'd08709ae5c5f6171',
