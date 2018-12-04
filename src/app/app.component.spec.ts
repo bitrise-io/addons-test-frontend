0013a091fcc04ecfa6921aabf1d784cfffdd3e73
József Erős <jozsef.eros@bitrise.io>
2018-12-04 17:32:29 +0100

    Test router outlet of app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, tick, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { InlineSVGModule } from 'ng-inline-svg';
 import { AppComponent } from './app.component';
@@ -12,19 +13,33 @@ import { AppComponent } from './app.component';
 class MockAppHeaderComponent {}
 
 @Component({
+  selector: 'mocked-router-outlet-component-a',
   template: ''
 })
-class MockedRouterOutletComponent {}
+class MockedRouterOutletComponentA {}
+
+@Component({
+  selector: 'mocked-router-outlet-component-b',
+  template: ''
+})
+class MockedRouterOutletComponentB {}
 
 describe('AppComponent', () => {
+  let router: Router;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         InlineSVGModule.forRoot(),
-        RouterTestingModule.withRoutes([{ path: 'mocked-route', component: MockedRouterOutletComponent }])
+        RouterTestingModule.withRoutes([
+          { path: 'mocked-route-a', component: MockedRouterOutletComponentA },
+          { path: 'mocked-route-b', component: MockedRouterOutletComponentB }
+        ])
       ],
-      declarations: [AppComponent, MockAppHeaderComponent, MockedRouterOutletComponent]
+      declarations: [AppComponent, MockAppHeaderComponent, MockedRouterOutletComponentA, MockedRouterOutletComponentB]
     }).compileComponents();
+
+    router = TestBed.get(Router);
   }));
 
   it('creates the app', async(() => {
@@ -38,5 +53,24 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const appHeader = fixture.debugElement.query(By.css('app-header'));
     expect(appHeader).not.toBeNull();
+  }));
+
+  it('renders component matching the current route', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    router.navigate(['mocked-route-a']);
+
+    tick();
+
+    const mockedRouterOutletComponentA = fixture.debugElement.query(By.css('mocked-router-outlet-component-a'));
+    expect(mockedRouterOutletComponentA).not.toBeNull();
+
+    router.navigate(['mocked-route-b']);
+
+    tick();
+
+    const mockedRouterOutletComponentB = fixture.debugElement.query(By.css('mocked-router-outlet-component-b'));
+    expect(mockedRouterOutletComponentB).not.toBeNull();
   }));
 });
