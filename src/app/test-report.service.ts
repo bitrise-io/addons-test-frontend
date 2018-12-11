3a8ddb5c16858661f931c37ad6f4a977c6899f8b
József Erős <jozsef.eros@bitrise.io>
2018-12-11 15:39:18 +0100

    Fix lint issues

--- src/app/test-report.service.ts
+++ src/app/test-report.service.ts
@@ -4,9 +4,30 @@ import { TestReport } from './test-report.model';
 @Injectable()
 export class TestReportService {
   private TEST_REPORTS = [
-    { id: 1, name: 'Unit Test A', inconclusiveTestSuiteCount: 5, passedTestSuiteCount: 3, failedTestSuiteCount: 2, skippedTestSuiteCount: 0 },
-    { id: 2, name: 'Unit Test X', inconclusiveTestSuiteCount: 3, passedTestSuiteCount: 2, failedTestSuiteCount: 0, skippedTestSuiteCount: 1 },
-    { id: 3, name: 'Unit Test Y', inconclusiveTestSuiteCount: 7, passedTestSuiteCount: 4, failedTestSuiteCount: 1, skippedTestSuiteCount: 3 }
+    {
+      id: 1,
+      name: 'Unit Test A',
+      inconclusiveTestSuiteCount: 5,
+      passedTestSuiteCount: 3,
+      failedTestSuiteCount: 2,
+      skippedTestSuiteCount: 0
+    },
+    {
+      id: 2,
+      name: 'Unit Test X',
+      inconclusiveTestSuiteCount: 3,
+      passedTestSuiteCount: 2,
+      failedTestSuiteCount: 0,
+      skippedTestSuiteCount: 1
+    },
+    {
+      id: 3,
+      name: 'Unit Test Y',
+      inconclusiveTestSuiteCount: 7,
+      passedTestSuiteCount: 4,
+      failedTestSuiteCount: 1,
+      skippedTestSuiteCount: 3
+    }
   ];
 
   getTestReports(): any[] {
