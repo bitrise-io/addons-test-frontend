75d59783b511fb2b367f8bf225d24698222b7ff0
József Erős <jozsef.eros@bitrise.io>
2018-12-12 15:54:08 +0100

    Revert other feature's leftover code from component's script

--- src/app/test-summary.component.ts
+++ src/app/test-summary.component.ts
@@ -1,18 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TestReportService } from './test-report.service';
-import { TestReport } from './test-report.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bitrise-test-summary',
   templateUrl: './test-summary.component.html',
   styleUrls: ['./test-summary.component.scss']
 })
-export class TestSummaryComponent implements OnInit {
-  testReports: TestReport[];
-
-  constructor(private testReportService: TestReportService) {}
-
-  ngOnInit() {
-    this.testReports = this.testReportService.getTestReports();
-  }
-}
+export class TestSummaryComponent {}
