1b98da58cbb25c79c7f2a3c7d43c5fe2ef834471
József Erős <jozsef.eros@bitrise.io>
2018-12-15 17:05:48 +0100

    Add unit test cases for test report model tests

--- src/app/test-report.model.spec.ts
+++ src/app/test-report.model.spec.ts
@@ -1,5 +1,6 @@
 import { TestReport } from './test-report.model';
 import { TestSuite, TestSuiteStatus } from './test-suite.model';
+import { TestCase, TestCaseStatus } from './test-case.model';
 
 describe('TestReport', () => {
   let testReport: TestReport;
@@ -9,7 +10,7 @@ describe('TestReport', () => {
   });
 
   describe('testsWithStatus', () => {
-    describe('when test report has some test suites', () => {
+    describe('when test report is a UI test report', () => {
       beforeEach(() => {
         testReport.testSuites = Array(6)
           .fill(null)
@@ -18,7 +19,7 @@ describe('TestReport', () => {
 
       [TestSuiteStatus.inconclusive, TestSuiteStatus.passed, TestSuiteStatus.failed, TestSuiteStatus.skipped].forEach(
         (status: TestSuiteStatus) => {
-          describe(`and some test suites has ${status} status`, () => {
+          describe(`and some test suites have ${status} status`, () => {
             beforeEach(() => {
               testReport.testSuites[0].status = status;
               testReport.testSuites[1].status = status;
@@ -31,6 +32,34 @@ describe('TestReport', () => {
               expect(testReport.testsWithStatus(status)).not.toContain(testReport.testSuites[3]);
               expect(testReport.testsWithStatus(status)).not.toContain(testReport.testSuites[4]);
               expect(testReport.testsWithStatus(status)).not.toContain(testReport.testSuites[5]);
+            });
+          });
+        }
+      );
+    });
+
+    describe('when test report is a unit test report', () => {
+      beforeEach(() => {
+        testReport.testCases = Array(6)
+          .fill(null)
+          .map(() => new TestCase());
+      });
+
+      [TestCaseStatus.passed, TestCaseStatus.failed].forEach(
+        (status: TestCaseStatus) => {
+          describe(`and some test cases have ${status} status`, () => {
+            beforeEach(() => {
+              testReport.testCases[0].status = status;
+              testReport.testCases[1].status = status;
+            });
+
+            it('returns only those test cases', () => {
+              expect(testReport.testsWithStatus(status)).toContain(testReport.testCases[0]);
+              expect(testReport.testsWithStatus(status)).toContain(testReport.testCases[1]);
+              expect(testReport.testsWithStatus(status)).not.toContain(testReport.testCases[2]);
+              expect(testReport.testsWithStatus(status)).not.toContain(testReport.testCases[3]);
+              expect(testReport.testsWithStatus(status)).not.toContain(testReport.testCases[4]);
+              expect(testReport.testsWithStatus(status)).not.toContain(testReport.testCases[5]);
             });
           });
         }
