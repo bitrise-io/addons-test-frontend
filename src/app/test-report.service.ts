686b6b2deaaa7481359165d0c288949ef6b359e6
József Erős <jozsef.eros@bitrise.io>
2018-12-15 15:57:49 +0100

    Strip mocked data

--- src/app/test-report.service.ts
+++ src/app/test-report.service.ts
@@ -15,25 +15,18 @@ export class TestReportService {
           status: TestCaseStatus.failed,
           durationInMilliseconds: 3000,
           context: 'ios_simple:dsfhkjshUITESTX',
-          summary: `Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+          summary: `Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis.
 
-Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.
+Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
 
-Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula porta felis euismod semper. Morbi leo risus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus muDuis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.`
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.`
         },
         {
           name: 'testCaseExample2',
-          status: TestCaseStatus.failed,
+          status: TestCaseStatus.passed,
           durationInMilliseconds: 5000,
           context: 'ios_complicated:abcdefgh',
           summary: 'Lorem ipsum'
-        },
-        {
-          name: 'testCaseExample3',
-          status: TestCaseStatus.passed,
-          durationInMilliseconds: 18000,
-          context: 'ios_simple:dsfhkjshUITESTX',
-          summary: 'Lorem ipsum etc.'
         }
       ]
     },
@@ -46,19 +39,6 @@ Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Lorem ip
           deviceName: 'iPhone 7',
           deviceOperatingSystem: 'iOS 11.2',
           durationInMilliseconds: 20000,
-          testCases: [
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed }
-          ],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'English'
-        },
-        {
-          status: TestSuiteStatus.passed,
-          deviceName: 'iPhone 8',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 300,
           testCases: [
             { status: TestCaseStatus.passed },
             { status: TestCaseStatus.passed },
@@ -86,7 +66,7 @@ Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Lorem ip
           deviceOperatingSystem: 'Android Oreo',
           durationInMilliseconds: 3700000,
           orientation: TestSuiteOrientation.landscape,
-          locale: 'Japanese'
+          locale: 'Traditional chinese'
         },
         {
           status: TestSuiteStatus.skipped,
@@ -95,101 +75,18 @@ Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Lorem ip
           durationInMilliseconds: 7200000,
           orientation: TestSuiteOrientation.landscape,
           locale: 'Japanese'
-        },
-        {
-          status: TestSuiteStatus.passed,
-          deviceName: 'iPhone 7',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 5000,
-          testCases: [
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed }
-          ],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'Japanese'
-        },
-        {
-          status: TestSuiteStatus.failed,
-          deviceName: 'iPhone 8',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 32000,
-          testCases: [{ status: TestCaseStatus.passed }, { status: TestCaseStatus.failed }],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'English'
         }
       ]
-    },
+    }
     {
       id: 3,
       name: 'UI Test B',
       testSuites: [
         {
-          status: TestSuiteStatus.skipped,
-          deviceName: 'iPhone 8',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 20,
-          testCases: [
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed }
-          ],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'Traditional chinese'
-        },
-        {
-          status: TestSuiteStatus.passed,
-          deviceName: 'iPad Mini 2016',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 2000,
-          testCases: [
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed }
-          ],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'Traditional chinese'
-        },
-        {
-          status: TestSuiteStatus.failed,
-          deviceName: 'iPad Mini 2017',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 120000,
-          testCases: [
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.failed },
-            { status: TestCaseStatus.failed }
-          ],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'English'
-        }
-      ]
-    },
-    {
-      id: 4,
-      name: 'UI Test C',
-      testSuites: [
-        {
-          status: TestSuiteStatus.passed,
-          deviceName: 'iPad Mini 2016',
-          deviceOperatingSystem: 'iOS 11.2',
-          durationInMilliseconds: 10,
-          testCases: [
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed },
-            { status: TestCaseStatus.passed }
-          ],
-          orientation: TestSuiteOrientation.portrait,
-          locale: 'English'
-        },
-        {
           status: TestSuiteStatus.passed,
           deviceName: 'Galaxy S4 mini',
           deviceOperatingSystem: 'Android Ice Cream Sandwich',
-          durationInMilliseconds: 1000,
+          durationInMilliseconds: 10,
           testCases: [
             { status: TestCaseStatus.passed },
             { status: TestCaseStatus.passed },
