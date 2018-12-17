46bb4fb3720883aedb2cf428cb0f975c11f43eb8
József Erős <jozsef.eros@bitrise.io>
2018-12-17 15:46:14 +0100

    Make changes prettier

--- src/app/test-report.service.ts
+++ src/app/test-report.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { TestReport, TestReportResponse } from './test-report.model';
-import { TestSuiteStatus, TestSuiteOrientation } from './test-suite.model';
-import { TestCaseStatus } from './test-case.model';
 import * as MOCKED_DATA from './mocked-data.json';
 
 @Injectable()
 export class TestReportService {
-  private TEST_REPORTS: any = (<any>MOCKED_DATA).test_reports;
+  private testReportResponses = (<any>MOCKED_DATA).test_reports;
 
   getTestReports(): TestReport[] {
-    return this.TEST_REPORTS.map((testReportResponse: TestReportResponse) =>
+    return this.testReportResponses.map((testReportResponse: TestReportResponse) =>
       new TestReport().deserialize(testReportResponse)
     );
   }
