63ff1e9e647b8d32b3695a7ebe19b04bda02d2ed
József Erős <jozsef.eros@bitrise.io>
2019-01-18 10:33:24 +0100

    Make details menu responsive

--- src/app/components/test-suite-details/test-suite-details.component.ts
+++ src/app/components/test-suite-details/test-suite-details.component.ts
@@ -1,28 +1,59 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'bitrise-test-suite-details',
   templateUrl: './test-suite-details.component.html',
   styleUrls: ['./test-suite-details.component.scss']
 })
-export class TestSuiteDetailsComponent {
-  testSuiteDetailsMenuItems = [{
-    name: 'Test Cases',
-    subpath: 'testcases'
-  }, {
-    name: 'Performance',
-    subpath: 'performance'
-  }, {
-    name: 'Video',
-    subpath: 'video'
-  }, {
-    name: 'Screenshots',
-    subpath: 'screenshots'
-  }, {
-    name: 'Test Artifacts',
-    subpath: 'testartifacts'
-  }, {
-    name: 'Logs',
-    subpath: 'logs'
-  }];
+export class TestSuiteDetailsComponent implements OnInit, OnDestroy {
+  activatedRouteParamsSubscription: Subscription;
+
+  testSuiteDetailsMenuItems = [
+    {
+      name: 'Test Cases',
+      subpath: 'testcases'
+    },
+    {
+      name: 'Performance',
+      subpath: 'performance'
+    },
+    {
+      name: 'Video',
+      subpath: 'video'
+    },
+    {
+      name: 'Screenshots',
+      subpath: 'screenshots'
+    },
+    {
+      name: 'Test Artifacts',
+      subpath: 'testartifacts'
+    },
+    {
+      name: 'Logs',
+      subpath: 'logs'
+    }
+  ];
+  selectedTestSuiteDetailsMenuItem: {
+    name: string;
+    subpath: string;
+  };
+
+  constructor(private location: Location, private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.selectedTestSuiteDetailsMenuItem = this.testSuiteDetailsMenuItems.find(
+      (testSuiteDetailsMenuItem: any) =>
+        testSuiteDetailsMenuItem.subpath === this.activatedRoute.firstChild.snapshot.routeConfig.path
+    );
+  }
+
+  ngOnDestroy() {}
+
+  selectedTestSuiteDetailsMenuItemChanged() {
+    this.router.navigate([`./${this.selectedTestSuiteDetailsMenuItem.subpath}`], { relativeTo: this.activatedRoute });
+  }
 }
