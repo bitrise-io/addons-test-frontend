9dc9c0f564ca45e796a1f52f954fe18f17e30e74
József Erős <jozsef.eros@bitrise.io>
2019-05-15 18:00:32 +0200

    Remove unnecessary stream

--- src/app/services/backend/backend.service.ts
+++ src/app/services/backend/backend.service.ts
@@ -45,7 +45,7 @@ export class RealBackendService implements BackendService {
           return forkJoin(
             testReport.testSuites.map((testSuite: TestSuite) => {
               if (testSuite.status === TestSuiteStatus.inProgress) {
-                return of({ testReport: testReport });
+                return of();
               } else {
                 if (testSuite.testCasesURL) {
                   return this.httpClient
