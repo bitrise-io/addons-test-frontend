eb647500280b6483e53e5a01f67aa0877a6e2520
Adam Pelle <adam.pelle@bitrise.io>
2020-04-03 10:19:58 +0200

    handle test case height change with v-scroller

--- src/app/components/test-suite-details/menu/test-cases/test-suite-details-menu-test-cases.component.ts
+++ src/app/components/test-suite-details/menu/test-cases/test-suite-details-menu-test-cases.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { VirtualScrollerComponent } from 'ngx-virtual-scroller';
 import { Subscription } from 'rxjs';
 
 import { TestReport } from 'src/app/models/test-report.model';
@@ -16,6 +17,13 @@ export class TestSuiteDetailsMenuTestCasesComponent implements OnInit, OnDestroy
   subscription: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute) {}
+
+  @ViewChild(VirtualScrollerComponent)
+  private virtualScroller: VirtualScrollerComponent;
+
+  updateListHeight(index: number) {
+    this.virtualScroller.invalidateCachedMeasurementAtIndex(index);
+  }
 
   ngOnInit() {
     this.subscription = this.activatedRoute.parent.data.subscribe(
